d208d58219cadbb429e35591669730a0e9290985
Luis Valenzuela <lvalenzuel2020@udec.cl>
2024-07-07 18:21:34 -0400

    Ordenando

--- client/rest_client.py
+++ client/rest_client.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 API_URL = 'http://localhost:3000/'
-    
+
 def get_db():
     try:
         response = requests.get(API_URL + 'db')
@@ -13,6 +13,4 @@ def get_db():
     except requests.exceptions.ConnectionError as e:
         print('Connection error...')
 
-
-if __name__ == '__main__':
-    get_db()
+get_db()
