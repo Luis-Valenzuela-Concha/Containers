841334a961e7e4566dead2f73a7acd751190f0fc
Luis Valenzuela <lvalenzuel2020@udec.cl>
2024-07-07 19:23:03 -0400

    ADD trigger

--- database/init-db.sql
+++ database/init-db.sql
@@ -5,3 +5,15 @@ CREATE TABLE connections (
     Sistema VARCHAR(100),
     Estado BOOLEAN
 );
+
+CREATE OR REPLACE FUNCTION last_tuple()
+RETURNS TRIGGER AS $$
+BEGIN
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER return_last_tuple
+AFTER INSERT ON connections
+FOR EACH ROW
+EXECUTE FUNCTION last_tuple();
