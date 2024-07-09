0ead3c20426121318e258413331dc54e1035d55f
Bruno Arce R. <brunoarce0@gmail.com>
2024-07-08 20:02:28 -0400

    RMQ ready

---
+++ client/rabbitmq_client.py
@@ -0,0 +1,29 @@
+import pika
+import json
+
+def send_message(message):
+    # Format the message into a JSON string
+    data = {
+        'text': message,
+        'system': 'RABBITMQ',
+        'status': True
+    }
+    json_message = json.dumps(data)
+
+    # Connect to RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Declare a queue
+    channel.queue_declare(queue='cola')
+
+    # Send a message
+    channel.basic_publish(exchange='', routing_key='cola', body=json_message)
+    print(f"Sent {json_message}")
+
+    # Close the connection
+    connection.close()
+
+if __name__ == '__main__':
+    message = "FINALMENTE!"
+    send_message(message)
---
+++ server/rabitmq_server.py
@@ -0,0 +1,50 @@
+import pika
+import psycopg2
+import os
+import json
+from datetime import datetime
+
+POSTGRES_DB = os.getenv('DB_NAME')
+POSTGRES_USER = os.getenv('DB_USER')
+POSTGRES_PASSWORD = os.getenv('DB_PASSWORD')
+POSTGRES_HOST = os.getenv('DB_HOST')
+
+def connect():
+    return psycopg2.connect(
+        dbname="mydb",
+        user="myuser",
+        password="mypassword",
+        host="localhost",
+        port=5432,
+    )
+
+def callback(ch, method, properties, body):
+    conn = connect()
+    cursor = conn.cursor()
+    print(f"Received {body}")
+
+    # Parse the JSON message
+    data = json.loads(body)
+
+    # Insert the manipulated data into the database
+    cursor.execute( "INSERT INTO connections (Texto, FechaHora, Sistema, Estado) VALUES (%s, %s, %s, %s)", (data['text'], datetime.now(), data['system'], data['status']))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def main():
+    # Connect to RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Declare a queue
+    channel.queue_declare(queue='cola')
+
+    # Consume messages from the queue
+    channel.basic_consume(queue='cola', on_message_callback=callback, auto_ack=True)
+
+    print('Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    main()
